33775a7528abd483523d30d916c7a99ea6a0520a
Okpanz <okpanz700@gmail.com>
2024-08-20 11:19:08 +0100

    API consumpution for Apply Discount

--- src/controller/revenue.controller.js
+++ src/controller/revenue.controller.js
@@ -133,11 +133,9 @@ module.exports = {
     }
   },
 
-
-  addDiscount: async function(req, res) {
+  addDiscount: async function (req, res) {
     const { invoice } = req.params;
-    const response = await revenue.initiateDiscount(invoice, req.body)
-    res.status(200).json(response)
-
-  }
+    const response = await revenue.initiateDiscount(invoice, req.body);
+    res.status(200).json(response);
+  },
 };

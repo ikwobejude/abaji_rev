8f7a08de56574cef0b17084e5d6fed7de8d1853c
Okpanz <okpanz700@gmail.com>
2024-09-01 20:53:35 +0100

    budpay integration

--- src/app.js
+++ src/app.js
@@ -4,16 +4,13 @@ const session = require("cookie-session");
 const compression = require("compression");
 const cors = require("cors");
 const path = require("path");
-const https = require('https');
-const xmlparser = require('express-xml-bodyparser');
+const https = require("https");
+const xmlparser = require("express-xml-bodyparser");
 https.globalAgent.maxSockets = Infinity;
-
 
 // const timeout = require("connect-timeout");
 
-
 const app = express();
-
 
 app.use(compression());
 app.use(cors());
@@ -22,14 +19,11 @@ app.use(cors());
 app.use(express.static("public"));
 app.use(express.static("uploads"));
 
-
 app.use(express.json({ limit: "50mb" })); //for json body parser
 app.use(cookieParser());
-app.use(xmlparser()); // req.rawBody Read XML 
+app.use(xmlparser()); // req.rawBody Read XML
 
 // app.use(timeout("180s"));
-
-
 
 //session setup
 app.set("trust proxy", 1); // trust first proxy
@@ -52,14 +46,11 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.route("*")
-  .get(function (req, res, next) {
-    res.locals.link = req.originalUrl.split('?')[0];
-    res.locals.copyright = new Date().getFullYear();
-    next();
-  })
-  
-
+app.route("*").get(function (req, res, next) {
+  res.locals.link = req.originalUrl.split("?")[0];
+  res.locals.copyright = new Date().getFullYear();
+  next();
+});
 
 const middleware = require("./middleware/middleware");
 const appRouter = require("./router/app.routes");
@@ -69,18 +60,18 @@ const adminRouter = require("./router/admin.routes");
 const setupRouter = require("./router/setup.routes");
 const walletRouter = require("./router/wallet.routes");
 const reportRouter = require("./router/report.routes");
-
+const budRouter = require("./router/budpay.routes");
 
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
-
-app.use('/', appRouter)
-app.use(authRouter)
-app.use('/api', apiRouter)
-app.use('/admin', middleware.requireAuth, adminRouter)
-app.use('/setup', middleware.requireAuth, setupRouter)
-app.use('/wallet', middleware.requireAuth, walletRouter)
-app.use('/report', middleware.requireAuth, reportRouter)
+app.use("/", appRouter);
+app.use(authRouter);
+app.use("/api", apiRouter);
+app.use("/admin", middleware.requireAuth, adminRouter);
+app.use("/setup", middleware.requireAuth, setupRouter);
+app.use("/wallet", middleware.requireAuth, walletRouter);
+app.use("/report", middleware.requireAuth, reportRouter);
+app.use("/pay", budRouter);
 
 module.exports = app;

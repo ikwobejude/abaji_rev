1713be4c3a6846c2c9cc30c9077caf1123242bdc
Okpanz <okpanz700@gmail.com>
2025-01-22 01:41:46 +0100

    updates

--- src/controller/setup.controller.js
+++ src/controller/setup.controller.js
@@ -213,11 +213,13 @@ module.exports = {
   },
   createBuildingType: async function (req, res) {
     try {
+      // console.log(req.body)
       await buildingService.addBuildingType(req.body);
       res
         .status(200)
         .json({ success: true, message: "Building Type created successfully" });
     } catch (error) {
+      console.log(error);
       res.status(500).json({
         success: false,
         message: error.message,
